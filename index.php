f83a0732fc23e3e334333843c8f16048fd4d2ff7
MarekStolz <stolzm95@gmail.com>
2023-01-22 20:41:45 +0100

    int

--- index.php
+++ index.php
@@ -21,26 +21,7 @@ mysqli_query($con, "SET CHARACTER SET UTF8");
 
 <body>
     <header>PRAGUE-AIRPORT</header>
-    <?php
-    $q = "SELECT * FROM flight";
-    $result = mysqli_query($con, $q);
-    echo '<table>' . PHP_EOL;
-    echo '<th>LET</th><th>ODLET</th><th>PRILET</th><th>DESTINACE</th><th>BRANA</th>' . PHP_EOL;
-    while (($airport = mysqli_fetch_array($result, MYSQLI_ASSOC)) !== null) {
-        echo '<tr><td>' . $airport['code'] . '</td>' . '</td>' . '<td>' . $airport['from_dttm'] . '</td>' . '<td>' . $airport['to_dttm'] . '</td>' . '<td>' . $airport['to_airport_code'] . '</td>' . '<td>' . $airport['gate_code'] . '</td>' . '</tr>' . PHP_EOL;
-    }
-    echo '</table>' . PHP_EOL;
-    /*
-        $airportS = [];
-        while (!empty($airportS = mysqli_fetch_array($result, MYSQLI_ASSOC)))
-         {
-            $airportS[] = [
-    'code' => $row['code'],
-    'id' => $row['id']        
-            ];
-}
-echo json_encode($airportS);*/
-    ?>
+    <?php include 'table.php';?>
 </body>
 
 </html>
