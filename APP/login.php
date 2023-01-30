44e3caa2b5ec76023f4d97ae646f6d121befd48c
Marek Stolz <81717582+MarekStolz@users.noreply.github.com>
2023-01-30 08:39:38 +0100

    Add files via upload

---
+++ APP/login.php
@@ -0,0 +1,9 @@
+<?php
+$con = @mysqli_connect("localhost", "root", "", "airport", "3306");
+if ($con === false) {
+    die("D8 error");
+}
+mb_internal_encoding('UTF-8');
+mysqli_query($con, "SET CHARACTER SET UTF8");
+
+?>
