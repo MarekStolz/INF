f4fe39e8419f5af9840c6cfa75cdd4f832f1523c
Marek Stolz <81717582+MarekStolz@users.noreply.github.com>
2023-01-30 08:38:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -15,20 +15,6 @@ require('login.php');
 
 <body>
     <header>PRAGUE-AIRPORT</header>
-    <script>
-        
-    function display_c(){
-var refresh=1000; // Refresh rate in milli seconds
-mytime=setTimeout('display_ct()',refresh)
-}
-    function display_ct() {
-var x = new Date()
-var x1=x.getMonth() + 1+ "/" + x.getDate() + "/" + x.getFullYear(); 
-x1 = x1 + " - " +  x.getHours( )+ ":" +  x.getMinutes() + ":" +  x.getSeconds();
-document.getElementById('ct').innerHTML = x1;
-display_c();
- }
-    </script>
     <script src="script.js"></script>
     <?php include 'table.php';?>
 </body>
