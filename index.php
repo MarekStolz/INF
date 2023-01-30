a7a016e9edc6658663c95cf227dd11e575c877b5
Marek Stolz <81717582+MarekStolz@users.noreply.github.com>
2023-01-30 08:38:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
-$con = @mysqli_connect("localhost", "root", "", "airport", "3306");
-if ($con === false) {
-    die("D8 error");
-}
-mb_internal_encoding('UTF-8');
-mysqli_query($con, "SET CHARACTER SET UTF8");
-
+require('login.php');
 ?>
 <!DOCTYPE html>
 <html lang="cz">
